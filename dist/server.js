c1d1614bd8456252c84c122200e0a3dc203a077a
David Shepherd <shepherdd1212@icloud.com>
2023-08-22 14:54:16 -0400

    prep for react

--- dist/server.js
+++ dist/server.js
@@ -5,11 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 const port = process.env.PORT;
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server is running.');
+    res.send('Hello World From the Typescript Server!');
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server iss running at http://localhost:${port}`);
